f56a62e329011c565426dea63d8a498ed5df121b
Valeria <valery0007@gmail.com>
2021-02-24 13:10:09 +0100

    axios and style row

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,19 +1,29 @@
+import React, { setState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Row from './Row';
+import requests from './requests';
+
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        h1 > Netflix Clone Front - end < /h1> <
-        Row title = "NETFLIX ORIGINALS" / >
-        <
-        Row title = "Trending Now" / >
+    return ( <div className = "App" >
+        <h1 > Netflix Clone Front - end </h1>
+        
+ <Row title = "Trending Now"
+        fetchUrl = { requests.fetchActionMovies }
+        isLargeRow
+        />
 
-        <
-        /
-        div >
+
+        </div >
     );
 }
 
-export default App;
+
+/* <Row title = "NETFLIX ORIGINALS"
+        fetchUrl = { requests.fetchNetflixOriginals } />
+          
+         <Row title = "Trending Now"
+        fetchUrl = { requests.fetchTrending }
+        /> */
+export default App
--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from './axios';
+import "./Row.css";
 
-function Row() {
-    return ( <
-        div >
+const base_url = "https://image.tmdb.org/t/p/original/";
 
-        <
-        /div>
+function Row({ title,  fetchUrl, isLargeRow }) {
+    const [movies, setMovies] = useState([]);
+
+    
+    //a snippet of code which runs based on a specific condition
+    useEffect(()=>{
+        //if [], run once when the row loads, and don t run again
+        async function fetchData(){
+            const request = await axios.get(fetchUrl);
+            console.log(request.data.results);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+    }, [fetchUrl]);
+    
+    console.log(movies);
+    
+    return ( < div className="row">
+        <h2 > { title } </h2> 
+   
+            <div className="row__posters">   {/*row__poster */}
+            
+            {movies.map(movie =>(<img 
+            key={movie.id}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+             src={`${base_url}${isLargeRow ? movie.poster_path :  movie.backdrop_path}`}
+             alt={movie.name}/>))}
+            </div>
+         
+        </div>
     )
 }
 
