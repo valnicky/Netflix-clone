3c84b5839da260266406df4d54592cde37a78b0c
Valeria <valery0007@gmail.com>
2021-03-01 13:11:09 +0100

    firebase

---
+++ netflix-clone/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDAH_SmL6RC-xPGu47mc_ar3ytZVciTnU8",
+    authDomain: "netflix-clone-de3ce.firebaseapp.com",
+    projectId: "netflix-clone-de3ce",
+    storageBucket: "netflix-clone-de3ce.appspot.com",
+    messagingSenderId: "228543441794",
+    appId: "1:228543441794:web:882f745819e8a24181bd9b",
+    measurementId: "G-WZ03CDN6M9"
+};
